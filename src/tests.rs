848d79fe138b045eac561c855cfeee2e08ffd679
Mikhail Rybakov <mikhail.rybakov@acronis.com>
2019-03-26 18:31:18 +0300

    Test 65536 values

--- src/tests.rs
+++ src/tests.rs
@@ -2,11 +2,12 @@ use super::md5::md5_rounds;
 use super::sha1::sha1_rounds;
 
 const MAX_ROUNDS: usize = 32;
+const MAX_DATA: u16 = 0xFFFF;
 
 pub fn run_md5_test_diff() {
-    let mut result: Vec<u32> = Vec::with_capacity(MAX_ROUNDS);
+    let mut result: Vec<u32> = Vec::with_capacity(MAX_ROUNDS+1);
     result.resize_with(MAX_ROUNDS+1, Default::default);
-    for input in 0x00000000u32..=0xFFFFFFFFu32 {
+    for input in 0..=MAX_DATA {
         let input = input.to_le_bytes();
         let mut previous_round_hash = md5_rounds(&input, 1);
         for rounds in 2..=MAX_ROUNDS {
@@ -18,14 +19,14 @@ pub fn run_md5_test_diff() {
 
     println!("run_md5_test_diff");
     for (round, diff) in result.iter().enumerate() {
-        println!("{}: {}", round, (*diff as f64) / (0xFFFFu32 as f64));
+        println!("{}: {}", round, (*diff as f64) / (MAX_DATA as f64));
     }
 }
 
 pub fn run_sha1_test_diff() {
-    let mut result: Vec<u32> = Vec::with_capacity(MAX_ROUNDS);
+    let mut result: Vec<u32> = Vec::with_capacity(MAX_ROUNDS+1);
     result.resize_with(MAX_ROUNDS+1, Default::default);
-    for input in 0x00000000u32..=0xFFFFFFFFu32 {
+    for input in 0..=MAX_DATA {
         let input = input.to_le_bytes();
         let mut previous_round_hash = sha1_rounds(&input, 1);
         for rounds in 2..=MAX_ROUNDS {
@@ -37,6 +38,6 @@ pub fn run_sha1_test_diff() {
 
     println!("run_sha1_test_diff");
     for (round, diff) in result.iter().enumerate() {
-        println!("{}: {}", round, (*diff as f64) / (0xFFFFu32 as f64));
+        println!("{}: {}", round, (*diff as f64) / (MAX_DATA as f64));
     }
 }
